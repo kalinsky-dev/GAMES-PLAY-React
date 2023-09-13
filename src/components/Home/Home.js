939d06c930b66beab6cda4f1b20437d23ff78efb
Kalin Hristov <kalin0169@gmail.com>
2023-09-13 15:12:07 +0300

    List latest games from the Server!

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import * as gameService from '../services/gameService';
+import LatestGame from "./LatestGame/LatestGame";
 
 const Home = () => {
+
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    gameService.getAll().then(result => {
+      setGames(result);
+      console.log(result);
+
+    });
+  }, []);
+
   return (
     <section id="welcome-world">
       <div className="welcome-message">
@@ -10,60 +24,7 @@ const Home = () => {
       <div id="home-page">
         <h1>Latest Games</h1>
         {/* Display div: with information about every game (if any) */}
-        <div className="game">
-          <div className="image-wrap">
-            <img src="./images/CoverFire.png" />
-          </div>
-          <h3>Cover Fire</h3>
-          <div className="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div className="data-buttons">
-            <a href="#" className="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
-        <div className="game">
-          <div className="image-wrap">
-            <img src="./images/ZombieLang.png" />
-          </div>
-          <h3>Zombie Lang</h3>
-          <div className="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div className="data-buttons">
-            <a href="#" className="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
-        <div className="game">
-          <div className="image-wrap">
-            <img src="./images/MineCraft.png" />
-          </div>
-          <h3>MineCraft</h3>
-          <div className="rating">
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-            <span>☆</span>
-          </div>
-          <div className="data-buttons">
-            <a href="#" className="btn details-btn">
-              Details
-            </a>
-          </div>
-        </div>
+        {games.map(x => <LatestGame game={x}></LatestGame>)}
         {/* Display paragraph: If there is no games  */}
         <p className="no-articles">No games yet</p>
       </div>
