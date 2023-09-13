6617e68484899d0ec6bad58b639f6c1a5a73c711
Kalin Hristov <kalin0169@gmail.com>
2023-09-13 13:43:55 +0300

    Create Home Component.

--- src/App.js
+++ src/App.js
@@ -1,81 +1,17 @@
-import Header from './components/Header/Header';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <div id="box">
+        {/*Header*/}
         <Header></Header>
         {/* Main Content */}
         <main id="main-content"></main>
         {/*Home Page*/}
-        <section id="welcome-world">
-          <div className="welcome-message">
-            <h2>ALL new games are</h2>
-            <h3>Only in GamesPlay</h3>
-          </div>
-          <img src="./images/four_slider_img01.png" alt="hero" />
-          <div id="home-page">
-            <h1>Latest Games</h1>
-            {/* Display div: with information about every game (if any) */}
-            <div className="game">
-              <div className="image-wrap">
-                <img src="./images/CoverFire.png" />
-              </div>
-              <h3>Cover Fire</h3>
-              <div className="rating">
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-              </div>
-              <div className="data-buttons">
-                <a href="#" className="btn details-btn">
-                  Details
-                </a>
-              </div>
-            </div>
-            <div className="game">
-              <div className="image-wrap">
-                <img src="./images/ZombieLang.png" />
-              </div>
-              <h3>Zombie Lang</h3>
-              <div className="rating">
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-              </div>
-              <div className="data-buttons">
-                <a href="#" className="btn details-btn">
-                  Details
-                </a>
-              </div>
-            </div>
-            <div className="game">
-              <div className="image-wrap">
-                <img src="./images/MineCraft.png" />
-              </div>
-              <h3>MineCraft</h3>
-              <div className="rating">
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-                <span>☆</span>
-              </div>
-              <div className="data-buttons">
-                <a href="#" className="btn details-btn">
-                  Details
-                </a>
-              </div>
-            </div>
-            {/* Display paragraph: If there is no games  */}
-            <p className="no-articles">No games yet</p>
-          </div>
-        </section>
+        <Home></Home>
         {/* Login Page ( Only for Guest users ) */}
         <section id="login-page" className="auth">
           <form id="login">
