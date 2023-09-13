7b4418b3313d9190980b30fdb7a6fc1305884c18
Kalin Hristov <kalin0169@gmail.com>
2023-09-13 13:26:34 +0300

    Create Header Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div id="box">
-        <header>
-          {/* Navigation */}
-          <h1>
-            <a className="home" href="#">
-              GamesPlay
-            </a>
-          </h1>
-          <nav>
-            <a href="#">All games</a>
-            {/* Logged-in users */}
-            <div id="user">
-              <a href="#">Create Game</a>
-              <a href="#">Logout</a>
-            </div>
-            {/* Guest users */}
-            <div id="guest">
-              <a href="#">Login</a>
-              <a href="#">Register</a>
-            </div>
-          </nav>
-        </header>
+        <Header></Header>
         {/* Main Content */}
         <main id="main-content"></main>
         {/*Home Page*/}
