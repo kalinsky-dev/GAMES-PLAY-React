121e8af4fa5e2e033a5b7fd4f6a1cd3fe2b18ae9
Kalin Hristov <kalin0169@gmail.com>
2023-09-14 14:41:58 +0300

    Create Game Component works!

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import CreateGame from './components/CreateGame/GreateGame';
 
 function App() {
   return (
@@ -19,56 +20,14 @@ function App() {
             <Route path='/' element={<Home />}></Route>
             <Route path='/login' element={<Login />}></Route>
             <Route path='/register' element={<Register />}></Route>
+            <Route path='/create' element={<CreateGame />}></Route>
 
           </Routes>
         </main>
 
 
 
-        {/* Create Page ( Only for logged-in users ) */}
-        {/* <section id="create-page" className="auth">
-          <form id="create">
-            <div className="container">
-              <h1>Create Game</h1>
-              <label htmlFor="leg-title">Legendary title:</label>
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="Enter game title..."
-              />
-              <label htmlFor="category">Category:</label>
-              <input
-                type="text"
-                id="category"
-                name="category"
-                placeholder="Enter game category..."
-              />
-              <label htmlFor="levels">MaxLevel:</label>
-              <input
-                type="number"
-                id="maxLevel"
-                name="maxLevel"
-                min={1}
-                placeholder={1}
-              />
-              <label htmlFor="game-img">Image:</label>
-              <input
-                type="text"
-                id="imageUrl"
-                name="imageUrl"
-                placeholder="Upload a photo..."
-              />
-              <label htmlFor="summary">Summary:</label>
-              <textarea name="summary" id="summary" defaultValue={""} />
-              <input
-                className="btn submit"
-                type="submit"
-                defaultValue="Create Game"
-              />
-            </div>
-          </form>
-        </section> */}
+
         {/* Edit Page ( Only for the creator )*/}
         {/* <section id="edit-page" className="auth">
           <form id="edit">
