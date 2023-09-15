71ae09b9093e52c15035493c70a31a952e46fd4d
Kalin Hristov <kalin0169@gmail.com>
2023-09-15 12:27:17 +0300

    Show Comments Conditionally if the persist or No comment if Not!

--- src/components/GameDetails/GameDetails.js
+++ src/components/GameDetails/GameDetails.js
@@ -26,7 +26,7 @@ const GameDetails = ({
   const onChange = (e) => {
 
     // console.log(`${e.target.name}:${e.target.value}`);
-    
+
     setComment(state => ({
       ...state,
       [e.target.name]: e.target.value
@@ -49,15 +49,17 @@ const GameDetails = ({
         <div className="details-comments">
           <h2>Comments:</h2>
           <ul>
-            <li className="comment">
-              <p>Content: I rate this one quite highly.</p>
-            </li>
-            <li className="comment">
-              <p>Content: The best game.</p>
-            </li>
+            {game.comments?.map(x =>
+              <li className="comment">
+                <p>{x}</p>
+              </li>
+            )}
           </ul>
-          <p className="no-comment">No comments.</p>
+          {!game.comments &&
+            <p className="no-comment">No comments.</p>
+          }
         </div>
+
         <div className="buttons">
           <a href="#" className="button">
             Edit
@@ -67,6 +69,7 @@ const GameDetails = ({
           </a>
         </div>
       </div>
+
       <article className="create-comment">
         <label>Add new comment:</label>
         <form className="form" onSubmit={addCommentHandler}>
