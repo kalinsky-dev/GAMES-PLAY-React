df843c9a81cc3d6d463be70d2ac4fbfc570a9411
Kalin Hristov <kalin0169@gmail.com>
2023-09-20 12:39:56 +0300

    Create Requester to fetch the Data from the Server and return it  to my Service.

---
+++ src/services/requester.js
@@ -0,0 +1,11 @@
+export const request = async (url) => {
+  try {
+    const response = await fetch(url);
+
+    const result = await response.json();
+    
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
