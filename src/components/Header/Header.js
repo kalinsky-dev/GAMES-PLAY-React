c11f6e920e4f4549592e93d6e03a9ff6e039b6cd
Kalin Hristov <kalin0169@gmail.com>
2023-09-22 12:03:47 +0300

    Use my useAuthContext() Custom Hook in Header Component!

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,34 @@
-import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
-import { AuthContext } from '../../contexts/AuthContext';
+import { useAuthContext } from '../../contexts/AuthContext';
 
 const Header = () => {
-    const { user } = useContext(AuthContext);
+  const { user } = useAuthContext();
 
-    return (
-        <header>
-            <h1>
-                <Link className="home" to="/">
-                    GamesPlay
-                </Link>
-            </h1>
-            <nav>
-                {user.email && <span>{user.email}</span>}
-                <Link to="/catalog">All games</Link>
-                {user.email
-                    ? <div id="user">
-                        <Link to="/create">Create Game</Link>
-                        <Link to="/logout">Logout</Link>
-                    </div>
-                    : <div id="guest">
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
-                    </div>
-                }
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <h1>
+        <Link className="home" to="/">
+          GamesPlay
+        </Link>
+      </h1>
+      <nav>
+        {user.email && <span>{user.email}</span>}
+        <Link to="/catalog">All games</Link>
+        {user.email ? (
+          <div id="user">
+            <Link to="/create">Create Game</Link>
+            <Link to="/logout">Logout</Link>
+          </div>
+        ) : (
+          <div id="guest">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
