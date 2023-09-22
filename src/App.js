0739686d933abe7010644ad050025da03e1e8c42
Kalin Hristov <kalin0169@gmail.com>
2023-09-22 11:18:39 +0300

    Import everything from AuthContext in AuthProvider!
    Context Pattern!

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState, lazy, Suspense } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import * as gameService from './services/gameService';
-import { AuthContext } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import { GameContext } from './contexts/GameContext';
 
 import Header from './components/Header/Header';
@@ -14,22 +14,12 @@ import EditGame from './components/EditGame/EditGame';
 import Catalog from './components/Catalog/Catalog';
 import GameDetails from './components/GameDetails/GameDetails';
 import './App.css';
-import { useLocalStorage } from './hooks/useLocalStorage';
 
 const Register = lazy(() => import('./components/Register/Register'));
 
 function App() {
   const [games, setGames] = useState([]);
-  const [auth, setAuth] = useLocalStorage('auth', {});
   const navigate = useNavigate();
-
-  const userLogin = (authData) => {
-    setAuth(authData);
-  };
-
-  const userLogout = () => {
-    setAuth({});
-  };
 
   const addComment = (gameId, comment) => {
     setGames((state) => {
@@ -60,41 +50,37 @@ function App() {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: auth, userLogin, userLogout }}>
-      <div className="App">
-        <div id="box">
-          {/*Header*/}
-          <Header />
-          {/* Main Content */}
-          <GameContext.Provider value={{ games, gameAdd, gameEdit }}>
-            <main id="main-content">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route
-                  path="/register"
-                  element={
-                    <Suspense fallback={<span>Loading....</span>}>
-                      <Register />
-                    </Suspense>
-                  }
-                />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/create" element={<CreateGame />} />
-                <Route path="/games/:gameId/edit" element={<EditGame />} />
-                <Route path="/catalog" element={<Catalog />} />
-                <Route
-                  path="/catalog/:gameId"
-                  element={
-                    <GameDetails games={games} addComment={addComment} />
-                  }
-                />
-              </Routes>
-            </main>
-          </GameContext.Provider>
-        </div>
+    <AuthProvider>
+      <div id="box">
+        {/*Header*/}
+        <Header />
+        {/* Main Content */}
+        <GameContext.Provider value={{ games, gameAdd, gameEdit }}>
+          <main id="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/register"
+                element={
+                  <Suspense fallback={<span>Loading....</span>}>
+                    <Register />
+                  </Suspense>
+                }
+              />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/create" element={<CreateGame />} />
+              <Route path="/games/:gameId/edit" element={<EditGame />} />
+              <Route path="/catalog" element={<Catalog />} />
+              <Route
+                path="/catalog/:gameId"
+                element={<GameDetails games={games} addComment={addComment} />}
+              />
+            </Routes>
+          </main>
+        </GameContext.Provider>
       </div>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
