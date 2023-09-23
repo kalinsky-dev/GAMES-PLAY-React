0c7206abe2c29f930b6b8cd824df004e55eb39da
Kalin Hristov <kalin0169@gmail.com>
2023-09-23 11:52:11 +0300

    Update commentService.js

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -1,13 +1,13 @@
-import * as request from "./requester";
+import * as request from './requester';
 
 const baseUrl = 'http://localhost:3030/data/comments';
 
 export const create = (gameId, comment) =>
-    request.post(baseUrl, { gameId, text: comment });
+  request.post(baseUrl, { gameId, text: comment });
 
 export const getByGameId = (gameId) => {
-    const relations = encodeURIComponent(`user=_ownerId:users`);
-    const search = encodeURIComponent(`gameId="${gameId}"`);
+  const relationQuery = encodeURIComponent(`user=_ownerId:users`);
+  const searchQuery = encodeURIComponent(`gameId="${gameId}"`);
 
-    return request.get(`${baseUrl}?where=${search}&load=${relations}`);
-}
+  return request.get(`${baseUrl}?where=${searchQuery}&load=${relationQuery}`);
+};
